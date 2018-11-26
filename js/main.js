95bf7578f5d203a0c8b2239b373d6fd50a81df73
Daniel W. Robert <danielwrobert@gmail.com>
2018-11-25 22:29:05 -0500

    Added fade-in effect using Intersection Observer API and the polyfill.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 ( function() {
-	console.log( 'The scripts have loaded. Tucker eats poo.' );
+	const images = document.querySelectorAll('.fade-in');
+
+	const observer = new IntersectionObserver((entries) => {
+		entries.forEach(entry => {
+			if (entry.intersectionRatio > 0) {
+				entry.target.classList.add('visible');
+			}
+		});
+	});
+
+	images.forEach(image => {
+		observer.observe(image);
+	});
 } )();
